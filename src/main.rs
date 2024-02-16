d5643c659cd64665dd8047bdbb497d05af5e95eb
Andrey Khranovsky <andrey.khranovsky@gmail.com>
2024-02-17 00:17:58 +0700

    Extract functuons

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::{
     sync::{Arc, Mutex},
 };
 
+use chrono::NaiveDateTime;
 use futures_util::FutureExt;
 use rust_socketio::{asynchronous::ClientBuilder, Event, Payload};
 use tokio::signal;
@@ -11,6 +12,7 @@ use tokio::signal;
 mod domain;
 mod services;
 
+use domain::Location;
 use services::{BusService, ConfigService, FetchService, RideService, RouteService};
 
 #[tokio::main]
@@ -46,57 +48,13 @@ async fn main() -> anyhow::Result<()> {
                     Event::Custom(custom) => match custom.as_ref() {
                         "sub_gps" => match payload {
                             Payload::Binary(bin) => println!("{custom} bin: {bin:?}"),
-                            Payload::String(value) => {
-                                let location =
-                                    match serde_json::from_str::<crate::domain::Location>(&value) {
-                                        Ok(value) => value,
-                                        Err(err) => {
-                                            eprintln!("ERROR Failed to parse, {err:#}\n{value}");
-                                            return;
-                                        }
-                                    };
-
-                                if let Some(last_date_time) = bus_lru.lock().unwrap().insert(location.car_license.clone(), location.date_time) {
-                                    if last_date_time == location.date_time {
-                                        // duplicating message, skip
-                                        return
-                                    }
-                                }
-
-                                let Some(bus) = bus_service.operate_position(&location.car_license)
-                                else {
-                                    eprintln!(
-                                        "WARN Non-operating bus, license={}",
-                                        location.car_license
-                                    );
-                                    return;
-                                };
-
-                                let Some(ride) = ride_service.get(bus, location.date_time.time())
-                                else {
-                                    eprintln!(
-                                        "WARN Non-operating bus, position={}, license={}",
-                                        bus, location.car_license
-                                    );
-                                    return;
-                                };
-
-                                if let Some((prev, next)) = route_service.locate(ride.direction(), location.coordinates) {
-                                println!(
-                                    "{}\t{}\t{} => {}, {}m from {} => {}m to {}, speed={}kmh, heading={}°, altitude={}m",
-                                    location.date_time,
-                                    ride.name, ride.start, ride.stop,
-                                    prev.coordinates.distance_to(location.coordinates), prev.name,
-                                    next.coordinates.distance_to(location.coordinates), next.name,
-                                    location.speed, location.heading.0, location.altitude
-                                );
-                                } else {
-                                eprintln!(
-                                    "WARN {}\t{} => {}, can't match location {}",
-                                    ride.name, ride.start, ride.stop,location.coordinates
-                                );
-                                }
-                            }
+                            Payload::String(value) => process_location_update(
+                                &value,
+                                &bus_lru,
+                                &bus_service,
+                                &ride_service,
+                                &route_service,
+                            ),
                         },
                         _ => println!("{custom}: {payload:?}"),
                     },
@@ -111,6 +69,68 @@ async fn main() -> anyhow::Result<()> {
     signal::ctrl_c().await?;
 
     Ok(())
+}
+
+fn process_location_update(
+    value: &str,
+    bus_lru: &Mutex<HashMap<String, NaiveDateTime>>,
+    bus_service: &BusService,
+    ride_service: &RideService,
+    route_service: &RouteService,
+) {
+    let location = match serde_json::from_str::<Location>(value) {
+        Ok(value) => value,
+        Err(err) => {
+            eprintln!("ERROR Failed to parse, {err:#}\n{value}");
+            return;
+        }
+    };
+
+    if let Some(last_date_time) = bus_lru
+        .lock()
+        .unwrap()
+        .insert(location.car_license.clone(), location.date_time)
+    {
+        if last_date_time == location.date_time {
+            // duplicating message, skip
+            return;
+        }
+    }
+
+    let Some(bus) = bus_service.operate_position(&location.car_license) else {
+        eprintln!("WARN Non-operating bus, license={}", location.car_license);
+        return;
+    };
+
+    let Some(ride) = ride_service.get(bus, location.date_time.time()) else {
+        eprintln!(
+            "WARN Non-operating bus, position={}, license={}",
+            bus, location.car_license
+        );
+        return;
+    };
+
+    if let Some((prev, next)) = route_service.locate(ride.direction(), location.coordinates) {
+        println!(
+            "{}\t{}\t{} => {}, {}m from {} => {}m to {}, speed={}kmh, heading={}°, altitude={}m",
+            location.date_time,
+            ride.name,
+            ride.start,
+            ride.stop,
+            prev.coordinates.distance_to(location.coordinates),
+            prev.name,
+            next.coordinates.distance_to(location.coordinates),
+            next.name,
+            location.speed,
+            location.heading.0,
+            location.altitude
+        );
+    } else {
+        eprintln!(
+            "WARN {}\t{} => {}, can't match location {}",
+            ride.name, ride.start, ride.stop, location.coordinates
+        );
+    }
 }
 
 fn fetch_test_data(config: &ConfigService) -> anyhow::Result<()> {
